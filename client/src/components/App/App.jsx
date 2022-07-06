740e502203ad9673056242471cffc9d47898e38d
Preeti Nagalamadaka <preetin@fb.com>
2022-07-05 22:47:58 -0700

    trying to override user stylesheet

---
+++ client/src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='app'>
+      <p className="work">work</p>
+      <h1 className='work'>work</h1>
+      <h1 className='work'>work</h1>
+    </div>
+  )
+}
+
+export default App
