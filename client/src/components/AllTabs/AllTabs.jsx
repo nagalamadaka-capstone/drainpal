2ed7f6770e213a38d799e3ff681afdb16d2d8828
Preeti Nagalamadaka <preetin@fb.com>
2022-07-27 10:17:34 -0700

    color graph added to tabs

--- client/src/components/AllTabs/AllTabs.jsx
+++ client/src/components/AllTabs/AllTabs.jsx
@@ -4,9 +4,20 @@ import { useState } from "react";
 
 import VolumeGraph from "../VolumeGraph/VolumeGraph";
 import TemplateGraph from "../DistressGraphs/TemplateGraph";
+import ColorGraph from "../ColorGraph/ColorGraph";
 
-function AllTabs({dataLogs}) {
-    const allTabs = ["Volume", "Pain", "Bowels", "Breathing", "Appetite", "Nausea", "Fatigue", "Sleeping"];
+function AllTabs({ dataLogs }) {
+  const allTabs = [
+    "volume",
+    "color",
+    "pain",
+    "bowels",
+    "breathing",
+    "appetite",
+    "nausea",
+    "fatigue",
+    "sleeping",
+  ];
   const [activeTab, setActiveTab] = useState("tab1");
   function handleTabClick(tab) {
     setActiveTab(tab);
@@ -15,35 +26,23 @@ function AllTabs({dataLogs}) {
     <div className="Tabs">
       <ul className="nav">
         {allTabs.map((tab) => (
-            <li key={tab}
-                className={tab === activeTab ? "active" : ""}
-                onClick={() => handleTabClick(tab)}
-            >
-                {tab}
-            </li>
+          <li
+            key={tab}
+            className={tab === activeTab ? "active" : ""}
+            onClick={() => handleTabClick(tab)}
+          >
+            {tab.charAt(0).toUpperCase() + tab.slice(1)}
+          </li>
         ))}
-
-
-
       </ul>
       <div className="outlet">
         {activeTab === allTabs[0] ? (
-          <VolumeGraph dataLogs={dataLogs}/>
+          <VolumeGraph dataLogs={dataLogs} />
         ) : activeTab === allTabs[1] ? (
-          <TemplateGraph dataLogs = {dataLogs} type = {"pain"}/>
-        ) : activeTab === allTabs[2] ? (
-            <TemplateGraph dataLogs = {dataLogs} type = {"bowels"}/>
-        ) : activeTab === allTabs[3] ? (
-            <TemplateGraph dataLogs = {dataLogs} type = {"breathing"}/>
-        ) : activeTab === allTabs[4] ? (
-            <TemplateGraph dataLogs = {dataLogs} type = {"appetite"}/>
-        ) : activeTab === allTabs[5] ? (
-            <TemplateGraph dataLogs = {dataLogs} type = {"nausea"}/>
-        ) : activeTab === allTabs[6] ? (
-            <TemplateGraph dataLogs = {dataLogs} type = {"fatigue"}/>
-        ) : activeTab === allTabs[7] ? (
-            <TemplateGraph dataLogs = {dataLogs} type = {"sleeping"}/>
-        ) : null}
+          <ColorGraph dataLogs={dataLogs} />
+        ) : (
+          <TemplateGraph dataLogs={dataLogs} type={activeTab} />
+        )}
       </div>
     </div>
   );
--- client/src/components/ColorGraph/ColorGraph.jsx
+++ client/src/components/ColorGraph/ColorGraph.jsx
@@ -1,70 +1,116 @@
-import React from 'react'
-import "./ColorGraph.css"
-import ReactApexChart from 'react-apexcharts';
+import React from "react";
+import "./ColorGraph.css";
+import ReactApexChart from "react-apexcharts";
 
-function ColorGraph({dataLogs}) {
-    const reversedDataLogs = [...dataLogs].reverse();
-    let categories = [];
-    let data = [];
-    reversedDataLogs.map(dataLog => {
-        categories.push(dataLog.date);
-        let hex = dataLog.color;
-        console.log('hex: ', hex);
-        let hsl = hexToHsl(hex);
-        console.log('hsl: ', hsl);
-        data.push(hsl);
+function ColorGraph({ dataLogs }) {
+  function hexToHsl(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
+
+    r = r / 255;
+    g = g / 255;
+    b = b / 255;
+
+    var max = Math.max(r, g, b),
+      min = Math.min(r, g, b);
+    var h,
+      s,
+      l = (max + min) / 2;
+
+    if (max == min) {
+      h = s = 0; // achromatic
+    } else {
+      var d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch (max) {
+        case r:
+          h = (g - b) / d + (g < b ? 6 : 0);
+          break;
+        case g:
+          h = (b - r) / d + 2;
+          break;
+        case b:
+          h = (r - g) / d + 4;
+          break;
+      }
+      h /= 6;
     }
-    );
 
-    let state = {
-          
-        series: [{
-            name: "Color",
-            data: data
-        }],
-        options: {
-          chart: {
-            height: 350,
-            type: 'line',
-            zoom: {
-              enabled: false
-            },
-          },
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            curve: 'straight'
-          },
-          colors: ['rgba(42, 63, 38, 0.65)'],
-          grid: {
-            row: {
-              colors: ['transparent'], // takes an array which will be repeated on columns
-              opacity: 0.5
-            },
-          },
-          xaxis: {
-            categories: categories,
-          },
-          yaxis: {
-            title: {
-              text: 'hsl color values:',
-            },
-            min: 0,
-          },
-          
+    s = s * 100;
+    s = Math.round(s);
+    l = l * 100;
+    l = Math.round(l);
+    h = Math.round(h * 360);
+
+    return h;
+  }
+
+  const reversedDataLogs = [...dataLogs].reverse();
+  let categories = [];
+  let data = [];
+  reversedDataLogs.map((dataLog) => {
+    categories.push(dataLog.date);
+    let hex = dataLog.drainColor;
+
+    let hsl = hexToHsl(hex);
+
+    data.push(hsl);
+  });
+
+  let state = {
+    series: [
+      {
+        name: "Color",
+        data: data,
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false,
         },
-      
-      
-      };
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "straight",
+      },
+      colors: ["rgba(42, 63, 38, 0.65)"],
+      grid: {
+        row: {
+          colors: ["transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+      xaxis: {
+        categories: categories,
+      },
+      yaxis: {
+        title: {
+          text: "hsl color values:",
+        },
+        min: 0,
+      },
+    },
+  };
 
   return (
-    <div className='colorGraph'>
-        <h2>Drain Color Volume Over Time</h2>
-        <ReactApexChart options={state.options} series={state.series} type="line" height={350} />
-    
+    <div className="colorGraph">
+      <h2>Drain Color Over Time</h2>
+      <ReactApexChart
+        options={state.options}
+        series={state.series}
+        type="line"
+        height={350}
+      />
     </div>
-  )
+  );
 }
 
-export default ColorGraph
+export default ColorGraph;
