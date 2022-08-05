7213ab4e30278564b126ac935f417f8be63753ea
nagalamadaka20p <77798750+nagalamadaka20p@users.noreply.github.com>
2022-08-05 13:41:36 -0700

    changed some navbar styling

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,56 +2,68 @@ import React from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
 
-function NavBar({ handleSignInOpen, handleCreateAccOpen, isLoggedIn, isDoctorLoggedIn }) {
+function NavBar({
+  handleSignInOpen,
+  handleCreateAccOpen,
+  isLoggedIn,
+  isDoctorLoggedIn,
+}) {
   return (
     <div className="navbar">
       <nav>
         {isLoggedIn && isDoctorLoggedIn ? (
           <ul>
-            <Link to="/">
-              <li id="DrainPalLi">
-                <h1 id="DrainPal">DrainPal</h1>
-              </li>
-            </Link>
-            <Link to="/profile">
-              <li className="menuhover">Profile</li>
-            </Link>
-            <Link to="/allpatients">
-              <li className="menuhover">View Patients</li>
-            </Link>
-            <Link to="/troubleshooting">
-              <li className="menuhover">Troubleshooting</li>
-            </Link>
-            <Link to="/">
-              <li className="menuhover">Home</li>
-            </Link>
+            <div className="navbar-container">
+              <Link to="/">
+                <li id="DrainPalLi">
+                  <h1 id="DrainPal">DrainPal</h1>
+                </li>
+              </Link>
+              <Link to="/profile">
+                <li className="menuhover">Profile</li>
+              </Link>
+              <Link to="/allpatients">
+                <li className="menuhover">View Patients</li>
+              </Link>
+              <Link to="/troubleshooting">
+                <li className="menuhover">Troubleshooting</li>
+              </Link>
+              <Link to="/">
+                <li className="menuhover">Home</li>
+              </Link>
+            </div>
           </ul>
-        ) 
-        : 
-        isLoggedIn && !isDoctorLoggedIn ? 
-        ( 
-            <ul>
-            <Link to="/">
-              <li id="DrainPalLi">
-                <h1 id="DrainPal">DrainPal</h1>
-              </li>
-            </Link>
-            <Link to="/profile">
-              <li className="menuhover">Profile</li>
-            </Link>
-            <Link to="/troubleshooting">
-              <li className="menuhover">Troubleshooting</li>
-            </Link>
-            <Link to="/data">
-              <li className="menuhover">Data Tracking</li>
-            </Link>
-            <Link to="/">
-              <li className="menuhover">Home</li>
-            </Link>
+        ) : isLoggedIn && !isDoctorLoggedIn ? (
+          <ul>
+            <div className="navbar-container">
+              <Link to="/">
+                <li id="DrainPalLi">
+                  <h1 id="DrainPal">DrainPal</h1>
+                </li>
+              </Link>
+              <Link to="/profile">
+                <li className="menuhover">
+                  <h2>Profile</h2>
+                </li>
+              </Link>
+              <Link to="/troubleshooting">
+                <li className="menuhover">
+                  <h2>Troubleshooting</h2>
+                </li>
+              </Link>
+              <Link to="/data">
+                <li className="menuhover">
+                  <h2>Data Tracking</h2>
+                </li>
+              </Link>
+              <Link to="/">
+                <li className="menuhover">
+                  <h2>Home</h2>
+                </li>
+              </Link>
+            </div>
           </ul>
-        ) 
-        : 
-        (
+        ) : (
           <ul>
             <Link to="/">
               <li>
