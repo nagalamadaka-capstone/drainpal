bdc18f749c1110daf0dfeb4d2db14bb8cbbb508d
nagalamadaka20p <77798750+nagalamadaka20p@users.noreply.github.com>
2022-08-08 11:14:17 -0700

    removed console.log

--- client/src/components/Home/Banner/TemplateBannerButton.jsx
+++ client/src/components/Home/Banner/TemplateBannerButton.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function TemplateBannerButton({linkAddress, buttonText, icon}) {
-    console.log('linkAddress: ', linkAddress);
+function TemplateBannerButton({ linkAddress, buttonText, icon }) {
   return (
     <div className="templateBanner">
       <Link to={linkAddress}>
-        
         <button>
           <div className="buttonFormat">
-            <div className="icon">
-              {icon}
-            </div>
-            <div className="notIcon">
-              {buttonText}
-            </div>
+            <div className="icon">{icon}</div>
+            <div className="notIcon">{buttonText}</div>
           </div>
         </button>
       </Link>
